b6cdb789eabccd0a91b7274e76996cc1ac4ff63e
Илья Вожжов <voggov1@mail.ru>
2022-04-10 17:36:33 +0300

    add RepeatType and Entity Inspection

--- src/main/java/com/coderiders/happyanimal/enums/RepeatType.java
+++ src/main/java/com/coderiders/happyanimal/enums/RepeatType.java
@@ -3,6 +3,10 @@ package com.coderiders.happyanimal.enums;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Getter
 @AllArgsConstructor
 public enum RepeatType {
@@ -12,5 +16,13 @@ public enum RepeatType {
 
 
     String string;
+
+    public List<String> getValues(){
+        return Arrays.stream(RepeatType.values()).map(value->value.getString()).collect(Collectors.toList());
+    }
+
+    public List<RepeatType> getValuesName(){
+        return Arrays.stream(RepeatType.values()).collect(Collectors.toList());
+    }
 
 }
