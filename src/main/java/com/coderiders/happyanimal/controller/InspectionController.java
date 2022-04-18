d7cd5dddfd41683ef5bdafc5dd4aecb5cc5287f4
egor.chelnokov <chelnokov16it18k@yandex.ru>
2022-04-18 11:50:33 +0300

    animal status as an enumeration

--- src/main/java/com/coderiders/happyanimal/controller/InspectionController.java
+++ src/main/java/com/coderiders/happyanimal/controller/InspectionController.java
@@ -5,7 +5,6 @@
 
 package com.coderiders.happyanimal.controller;
 
-import com.coderiders.happyanimal.model.dto.InspectionRqDto;
 import com.coderiders.happyanimal.model.dto.InspectionRsDto;
 import com.coderiders.happyanimal.service.InspectionService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -13,39 +12,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping({"/inspection"})
 public class InspectionController {
-    private final InspectionService service;
+    private final InspectionService inspectionService;
 
     @Autowired
     public InspectionController(InspectionService service) {
-        this.service = service;
+        this.inspectionService = service;
     }
 
-    @Operation(summary = "Добавление осмотра")
-    @PostMapping(produces = {"application/json"})
-    public ResponseEntity<InspectionRsDto> addInspection(@Validated @RequestBody InspectionRqDto dto) {
-        InspectionRsDto created = this.service.saveInspection(dto);
-        URI url = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(created.getId()).toUri();
-        return ResponseEntity.created(url).body(created);
+    @Operation(summary = "Записать животное на осмотр")
+    @PutMapping(produces = {"application/json"})
+    public ResponseEntity<Void> addAnimalToInspection(Long animalId, LocalDate localDate) {
+        inspectionService.addAnimalToInspection(animalId, localDate);
+        return ResponseEntity.ok().build();
     }
 
     @Operation(summary = "Все запланированные осмотры")
     @GetMapping(produces = {"application/json"})
     public Page<InspectionRsDto> getAll(Pageable pageable) {
-        return this.service.getAll(pageable);
+        return this.inspectionService.getAll(pageable);
     }
 
     @Operation(summary = "Осмотр по id")
     @GetMapping(path = {"/{id}"})
     public InspectionRsDto getById(@PathVariable Long id) {
-        return this.service.getById(id);
+        return this.inspectionService.getById(id);
     }
 }
--- src/main/java/com/coderiders/happyanimal/service/InspectionService.java
+++ src/main/java/com/coderiders/happyanimal/service/InspectionService.java
@@ -1,10 +1,12 @@
 package com.coderiders.happyanimal.service;
 
+import com.coderiders.happyanimal.enums.AnimalStatus;
 import com.coderiders.happyanimal.exceptions.NotFoundException;
 import com.coderiders.happyanimal.mapper.InspectionMapper;
+import com.coderiders.happyanimal.model.Animal;
 import com.coderiders.happyanimal.model.Inspection;
-import com.coderiders.happyanimal.model.dto.InspectionRqDto;
 import com.coderiders.happyanimal.model.dto.InspectionRsDto;
+import com.coderiders.happyanimal.repository.AnimalRepository;
 import com.coderiders.happyanimal.repository.InspectionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -12,24 +14,26 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
+
 @Service
 public class InspectionService {
     private final InspectionRepository inspectionRepository;
     private final InspectionMapper inspectionMapper;
+    private final AnimalRepository animalRepository;
 
     @Autowired
-    public InspectionService(InspectionRepository inspectionRepository, InspectionMapper inspectionMapper) {
+    public InspectionService(InspectionRepository inspectionRepository,
+                             InspectionMapper inspectionMapper,
+                             AnimalRepository animalRepository) {
         this.inspectionRepository = inspectionRepository;
         this.inspectionMapper = inspectionMapper;
+        this.animalRepository = animalRepository;
     }
 
-    @Transactional
-    public InspectionRsDto saveInspection(InspectionRqDto dto) {
-        Inspection inspection = this.inspectionMapper.mapToInspection(dto);
-        this.inspectionRepository.save(inspection);
-        return this.inspectionMapper.mapToRsDto(inspection);
-    }
 
     @Transactional
     public InspectionRsDto getById(Long id) {
@@ -39,13 +43,36 @@ public class InspectionService {
 
     @Transactional
     public Page<InspectionRsDto> getAll(Pageable pageable) {
-        Page<Inspection> allInspection = this.inspectionRepository.findAll(pageable);
+        Page<Inspection> allInspection = inspectionRepository.findAll(pageable);
         if (allInspection.isEmpty()) {
             throw new NotFoundException("Запланированных осмотров нет!");
         } else {
-            InspectionMapper var10001 = this.inspectionMapper;
+            InspectionMapper var10001 = inspectionMapper;
             Objects.requireNonNull(var10001);
             return allInspection.map(var10001::mapToRsDto);
+        }
+    }
+
+    @Transactional
+    public void addAnimalToInspection(Long animalId, LocalDate localDate) {
+        Optional<Inspection> inspectionOpt = inspectionRepository.findByDate(localDate);
+        if (inspectionOpt.isPresent()) {
+            Inspection inspection = inspectionOpt.get();
+            List<Animal> animalList = inspection.getAnimalList();
+            Animal animal = animalRepository.getById(animalId);
+            if (!animalList.contains(animal)) {
+                animalList.add(animal);
+                inspection.setAnimalList(animalList);
+                inspectionRepository.save(inspection);
+                animal.setStatus(AnimalStatus.BOOKED);
+                animalRepository.save(animal);
+            }
+        } else {
+            inspectionRepository.save(Inspection.builder()
+                    .date(localDate)
+                    .animalList(List.of(animalRepository.getById(animalId)))
+                    .build()
+            );
         }
     }
 }
