ef1cbeb02e30e3d49598c9e1cfad5b4167f8759e
Илья Вожжов <voggov1@mail.ru>
2022-05-05 22:14:24 +0300

    тесты репо

--- src/test/java/com/coderiders/happyanimal/rep/InspectionRepTest.java
+++ src/test/java/com/coderiders/happyanimal/rep/InspectionRepTest.java
@@ -1,7 +1,6 @@
 package com.coderiders.happyanimal.rep;
 
-import com.coderiders.happyanimal.enums.AnimalStatus;
-import com.coderiders.happyanimal.model.*;
+import com.coderiders.happyanimal.model.Inspection;
 import com.coderiders.happyanimal.repository.InspectionRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,9 +10,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -29,29 +26,6 @@ public class InspectionRepTest {
     @Autowired
     private InspectionRepository inspectionRepository;
 
-    AnimalKind animalKind1 = new AnimalKind("Жираф", "Млекопитающие", "dsfsdfsmfk", "Парнокопытные".getBytes(StandardCharsets.UTF_8));
-
-    User user = User.builder()
-            .age(47)
-            .name("Иван")
-            .build();
-
-
-    Animal animal = Animal.builder()
-            .name("Шарик")
-            .gender("male")
-            .age(15)
-            .height(155)
-            .weight(228)
-            .animalKind(animalKind1)
-            .featuresOfKeeping("Пятно на лбу")
-            .externalFeatures("закрытый вальер")
-            .status(AnimalStatus.FINE)
-            .location("Вальер1")
-            .user(user)
-            .build();
-    Inspection inspection = Inspection.builder().date(LocalDate.now()).animalList(Arrays.asList(animal)).build();
-
     @Test
     @Transactional
     void findAll() {
@@ -63,20 +37,15 @@ public class InspectionRepTest {
     @Test
     @Transactional
     void findById() {
-        entityManager.persist(animalKind1);
-        entityManager.persist(animal);
-        Inspection persist = entityManager.persist(inspection);
-        assertEquals(inspectionRepository.findById(1L).orElse(inspection), persist);
+        Inspection persist = entityManager.persist(new Inspection());
+        assertEquals(inspectionRepository.findById(1L).orElse(persist), persist);
     }
 
     @Test
     @Transactional
-    void findByDate(){
-        entityManager.persist(user);
-        entityManager.persist(animalKind1);
-        entityManager.persist(animal);
-        Inspection persist = entityManager.persist(inspection);
-        assertEquals(inspectionRepository.findByDate(LocalDate.now()).orElse(inspection), persist);
+    void findByDate() {
+        Inspection persist = entityManager.persist(Inspection.builder().date(LocalDate.now()).build());
+        assertEquals(inspectionRepository.findByDate(LocalDate.now()).orElse(persist), persist);
 
     }
 }
